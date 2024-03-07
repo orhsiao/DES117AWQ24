6eb41399ab58bbd8248708959f6a17159c6ab5ac
ophelia_hsiao <orhsiao@ucdavis.edu>
2024-03-07 02:31:20 -0800

    added

--- studio3/script.js
+++ studio3/script.js
@@ -2,4 +2,21 @@
     'use strict';
     console.log('Reading JS');
     
+    //event handler for each image square 'click' get element class (fish noFish)
+    // array. 4 possible. if array has 4 then correct.
+
+    //click submit. event listener if array.length==4 then "Congrats"
+
+    //4 sets of 4 images 
+    //compare selected images w/array answers (image id)
+
+    //feedback for matched images
+
+    //if correct "congrats" if incorrect "try again"
+
+    //"button" check 4 selected, and if correct array = empty and remove event listener from images no click
+
+    //button click only if 4 images selected
+
+    //icons from fontawesome, categories, illustrations/images, typeface, color theory
 })();
